017bdc2f4b6394dc861ae74e2c1972081827fb62
Aidan Wood <aiwo9411@colorado.edu>
2024-03-03 12:56:40 -0700

    Add initial bad RGV controller

--- scripts/rgv_controller.py
+++ scripts/rgv_controller.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python3
 import rospy
 import sys
-from geometry_msgs.msg import Twist
+from gazebo_msgs.srv import SetModelState, SetModelStateRequest, SetModelStateResponse
+from scipy.spatial.transform import Rotation
+from geometry_msgs.msg import Quaternion
+import numpy as nd
 
-if len(sys.argv) != 2:
-    print("Specify name of RGV model as command line argument")
+
+UPDATE_RATE = 30
+SPEED = 0.5
+
+DX = SPEED/UPDATE_RATE
+
+def fillQuaternionMessage(msg: Quaternion, values: nd.ndarray):
+    msg.x = values[0]
+    msg.y = values[1]
+    msg.z = values[2]
+    msg.w = values[3]
+
+
+if len(sys.argv) < 5:
+    print("Usage: rgv_controller.py <RGV_NAME> <X> <Y> <YAW>")
     exit(-1)
 
 rgv_name = sys.argv[1]
-print(f"rgv_controller_{rgv_name}")
 rospy.init_node(f"rgv_controller_{rgv_name}")
+rgv_x0 = float(sys.argv[2])
+rgv_y0 = float(sys.argv[3])
+rgv_Y0 = float(sys.argv[4])
     
-twist_pub = rospy.Publisher("gazebo/set_model_state", Twist, queue_size=1)
-rate = rospy.Rate(1)
+rospy.wait_for_service("gazebo/set_model_state")
+mover_service_proxy = rospy.ServiceProxy("gazebo/set_model_state", SetModelState)
+rate = rospy.Rate(UPDATE_RATE)
 
-msg = Twist()
-msg.linear.x = 1
-msg.angular.z = 1
+rqst = SetModelStateRequest()
+rqst.model_state.model_name = rgv_name
+rqst.model_state.pose.position.x = rgv_x0
+rqst.model_state.pose.position.y = rgv_y0
+quat = Rotation.from_euler("xyz", [0, 0, rgv_Y0]).as_quat()
+fillQuaternionMessage(rqst.model_state.pose.orientation, quat)
 
 while not rospy.is_shutdown():
-    twist_pub.publish(msg)
-    msg.angular.z = -msg.angular.z
+    resp: SetModelStateResponse = mover_service_proxy(rqst)
+    if not resp.success:
+        print(resp.status_message)
+    rqst.model_state.pose.position.x += DX
     rate.sleep()
